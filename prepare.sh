09ca7e0a3113a3083918be61c9e5e23144ae5769
Noel Austin <naustin00@gmail.com>
2017-09-18 09:00:28 -0400

    updates for 2016

--- prepare.sh
+++ prepare.sh
@@ -3,8 +3,10 @@
 cd $(dirname "$0")
 cd build/install
 
-if [ ! -f "ColdFusion_11_WWEJ_linux64.bin" ]
+if [ ! -f "ColdFusion_2016_WWEJ_linux64.bin" ]
 then
-	wget http://trials3.adobe.com/AdobeProducts/CSTD/11/linux64/ColdFusion_11_WWEJ_linux64.bin
-	chmod 755 ColdFusion_11_WWEJ_linux64.bin
+	wget http://trials3.adobe.com/AdobeProducts/CSTD/11/linux64/ColdFusion_2016_WWEJ_linux64.bin
+	
 fi
+
+chmod 755 ColdFusion_2016_WWEJ_linux64.bin
